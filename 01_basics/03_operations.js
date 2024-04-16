2bd69e52f8f56a41b7ebba8c1231985a6f837b48
ShalvSingh <113675503+ShalvSingh@users.noreply.github.com>
2024-04-16 00:48:04 +0000

    other conversion

---
+++ 01_basics/03_operations.js
@@ -0,0 +1,73 @@
+// ***********************************operations**********************************************
+
+let value =3
+let negvalue =-value
+//console.log(negvalue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "hello"
+let str2 = "Aman"
+
+let str3 = str1 + str2;
+console.log(str3)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + "2")
+console.log("1" + 2 + 2)  // if string 1st then all conversion in string
+console.log( 2 + 2 + "1")//  if num then all in num
+
+
+console.log(true)
+console.log(+true)
+
+console.log(+"")
+
+let num1, num2, num3 
+
+let gameCoun = 100
+console.log(gameCoun)
+
+console.log("2" > 1)
+console.log("02" > 1)
+
+console.log(null > 0)
+console.log(null == 0)  // js converts null in 0 and is 0==0 , no false
+console.log(null >= 0)
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+// ===
+console.log(undefined === 0) // directly checks data type 1st
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
